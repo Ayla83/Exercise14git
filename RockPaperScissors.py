082eb3fddef87a98e3a059d68115923f681ca156
Ayla <aylafertug@hotmail.com>
2022-02-27 13:40:25 +0000

    function for game and while loop to continue playing with score counter

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,106 +1,73 @@
-# User will play against computer
-# Design a program which:
-# Prompts the user to enter a value R, P, or S
-# Program should convert this value into Rock, Paper or Scissors respectively
-# Ask the computer to generate a random value between 0 and 2
-# Convert the computer's choice: 0 becomes Rock, 1 becomes Paper, 2 becomes Scissors'
-# Compare user's choice with computer's choice to display a message indicating whether the user won, lost or drew
-# against computer
-
-# Showcase what you have learned about conditional statements and create your own functions
-
-# Still to do: add in conditionals and functions
-
 from random import randint
 
-# user inputs their choice
-userinput = input("Choose Rock (R), Paper(P) or Scissors(S)! ").upper()
-
-# reassign choice of letter to game object
-if userinput == "R":
-    userhand = "rock"
-elif userinput == "P":
-    userhand = "paper"
-elif userinput == "S":
-    userhand = "scissors"
-else:
-    print("You didn't choose a hand!  Try again")
-
-print("userhand is: ", userhand)
-
-# Create a random number for the computer's choice
-comprand = randint(0,2)
-
-if comprand == 0:
-    computerhand = "rock"
-elif comprand == 1:
-    computerhand = "paper"
-elif comprand == 2:
-    computerhand = "scissors"
-else:
-    print("Something went wrong")
-
-print("computerhand is: ", computerhand)
-
-# Put together the two opponents in a set
-gamelist = {userhand, computerhand}
-
-print("The game is: ", gamelist)
-
-# Define the general sets of play
-rockpaper = {"rock", "paper"}
-paperscissors = {"paper", "scissors"}
-scissorsrock = {"scissors", "rock"}
-
-# big loop for checking who wins
-if gamelist == rockpaper:
-    for item in gamelist:
-        if item == "paper":                 # rock v paper, paper wins
-            print(item, "covers rock,")
-            if item == userhand:            # print who won
-                print("user wins!")
-            elif item == computerhand:
-                print("computer wins")
-            else:
-                print("something wrong 1")
-        else:
-            pass
-elif gamelist == paperscissors:
-    for item in gamelist:
-        if item == "scissors":              # paper v scissors, scissors wins
-            print(item, "cut paper,")
-            if item == userhand:            # print who won
-                print("user wins!")
-            elif item == computerhand:
-                print("computer wins")
-            else:
-                print("something wrong 2")
-        else:
-            pass
-elif gamelist == scissorsrock:
-    for item in gamelist:
-        if item == "rock":                  # scissors v rock, rock wins
-            print(item, "blunts scissors,")
-            if item == userhand:            # print who won
-                print("user wins!")
-            elif item == computerhand:
-                print("computer wins")
-            else:
-                print("something wrong 3")
-        else:
-            pass
-elif len(gamelist)==1:                      # if userhand = computerhand, it's a tie
-    print("It's a tie!  Try again?")
-else:
-     print("Something else went wrong")
+def game(userhand, computerhand):
+    if userhand == computerhand:
+        print("It's a tie!")
+    elif userhand == "rock":
+        if computerhand == "paper":
+            print("paper covers rock,")
+            print("computer wins - you lose!")
+            return 1                                # function returns 1 if computer wins, or 2 if user wins
+        elif computerhand == "scissors":
+            print("rock blunts scissors,")
+            print("you win!")
+            return 2
+    elif userhand == "paper":
+        if computerhand == "scissors":
+            print("scissors cut paper,")
+            print("computer wins - you lose!")
+            return 1
+        elif computerhand == "rock":
+            print("paper covers rock,")
+            print("you win!")
+            return 2
+    elif userhand == "scissors":
+        if computerhand == "rock":
+            print("rock blunts scissors,")
+            print("computer wins - you lose!")
+            return 1
+        elif computerhand == "paper":
+            print("scissors cut paper,")
+            print("you win!")
+            return 2
 
 
+userscore = 0
+compscore = 0
 
+while True:
+    userinput = input("Choose Rock (R), Paper (P) or Scissors (S)! ").upper()  # user inputs their choice
+    if userinput == "R":
+        userhand = "rock"
+    elif userinput == "P":
+        userhand = "paper"
+    elif userinput == "S":
+        userhand = "scissors"
+    else:
+        print("You didn't choose a hand!  Try again")
 
-# Compare to other class members:
-# Have we achieved this differently?
-# Does anyone's solution stand out?
-# How would you structure your code differently?
-# Would you consider changing the names of my variables or functions?
+    print("You chose: ", userhand)
 
+    comprand = randint(0, 2)                # Create a random number for the computer's choice
+    if comprand == 0:
+        computerhand = "rock"
+    elif comprand == 1:
+        computerhand = "paper"
+    elif comprand == 2:
+        computerhand = "scissors"
+    else:
+        print("Something went wrong")
 
+    print("Computer chose: ", computerhand)
+
+    result = game(userhand, computerhand)            # check who wins
+    if result == 1:
+        compscore +=1
+    elif result == 2:
+        userscore +=1
+    print("User:", userscore, "Computer:", compscore)
+
+    playagain = input("Play again? Y/N: ").upper()
+    if playagain == "N":
+        print("Thanks for playing!")
+        break
